70a574bbd2251de9d6e8baaaff96d81f0bd93720
yoyo0527 <C110156132@nkust.edu.tw>
2024-04-18 13:37:29 +0800

    0418

--- src/components/about.js
+++ src/components/about.js
@@ -4,9 +4,22 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import img1 from '../assets/images/intro.png';
+import { useEffect, useState } from 'react';
 
 function AppAbout() {
-  return (
+  const [result, setResult] = useState('');
+
+  useEffect(() => {
+    // 在組件首次渲染時發送 GET 請求
+    fetch('http://127.0.0.1:5000/recognize-nail')
+      .then(response => response.json())
+      .then(data => {
+        setResult(data.result); // 更新組件狀態，將結果保存下來
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);  return (
     <section id="about" className="block about-block">
       <Container fluid>
         <div className="title-holder">
@@ -18,6 +31,7 @@ function AppAbout() {
           </Col>
           <Col sm={6} style={{display:'flex',alignItems:'center',lineHeight:2}}>
             <p>手腳指甲是人體健康的一個指標，指甲的外觀可以反映出身體健康狀況的一些跡象。然而，許多人可能未能及時察覺或了解指甲變化的​重要性，因此這項應用的開發是為了幫助用戶更容易地識別並了解可能與指甲健康相關的​問題。​</p>
+            <p>{result}</p>
           </Col>
         </Row>
       </Container>
